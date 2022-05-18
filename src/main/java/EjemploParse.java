19f2487909571656efeca25c6f94b87451a41a5a
david braga rodrigues <dbragarodrigues@danielcastelao.org>
2022-05-18 13:48:16 +0200

    nueva rama creada http request

--- src/main/java/EjemploParse.java
+++ src/main/java/EjemploParse.java
@@ -1,24 +1,39 @@
+import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class EjemploParse {
-    public static void main(String args[]) {
-        String cadenaJSON = "{" +
-                "    \"data\": {" +
-                "        \"id\": 2," +
-                "        \"email\": \"janet.weaver@reqres.in\"," +
-                "        \"first_name\": [\"Janet\", \"Brown\"]," +
-                "        \"vivo\": false," +
-                "        \"avatar\": \"https://reqres.in/img/faces/2-image.jpg\""+
-                "    }," +
-                "    \"support\": {" +
-                "       \"url\": \"https://reqres.in/#support-heading\"," +
-                "       \"text\": \"To keep ReqRes free, contributions towards server costs are appreciated!\""+
-                "    }" +
-                "}";
+    public static void main(String args[]) throws URISyntaxException, IOException, InterruptedException {
 
-        JSONObject jo = new JSONObject(cadenaJSON);
+        // creamos la petición
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI("https://jsonplaceholder.typicode.com/todos/1"))
+                .GET()
+                .build();
 
-        Boolean correo = jo.getJSONObject("data").getBoolean("vivo");
+        // creamos el cliente
+        HttpClient client = HttpClient.newHttpClient();
 
-        System.out.println(correo);
+        // Enviamos la petición y obtenemos la respuesta
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        // Mostramos el cuerpo de la respuesta
+        System.out.println(response.body());
+
+        /*String cadenaJSON = "";
+        JSONObject obj = new JSONObject(cadenaJSON);
+        String vivo = obj.getJSONObject("pageInfo").getString("pageName");
+        JSONArray arr = obj.getJSONArray("posts");
+        for (int i = 0; i < arr.length(); i++) {
+            String post_id = arr.getJSONObject(i).getString("post_id");
+            System.out.println(post_id);
+        }
+        System.out.println(vivo);*/
     }
 }
